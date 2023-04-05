aab7881d032ee006365ffc673457e4e3bb95f388
Hassan Elqadi <hassanelqadi2@gmail.com>
2023-04-05 02:49:28 +0000

    init

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-var today = new Date();
+function updateClock() {
 
-var seconds = today.getSeconds()<10 ? '0'+today.getSeconds() : today.getSeconds();
-var hours = today.getHours()<10 ? '0'+today.getHours() : today.getHours();
-var minutes = today.getMinutes()<10 ? '0'+today.getMinutes() : today.getMinutes();
-var format = today.getHours()>= 12 ? "PM" : "AM"
+    var today = new Date();
 
-document.getElementById("seconds").innerHTML = seconds;
-document.getElementById("minutes").innerHTML = minutes;
-document.getElementById("hours").innerHTML = hours;
-document.getElementById("format").innerHTML = format;
+    var seconds = today.getSeconds()<10 ? '0'+today.getSeconds() : today.getSeconds();
+    var hours = today.getHours()<10 ? '0'+today.getHours() : today.getHours();
+    var minutes = today.getMinutes()<10 ? '0'+today.getMinutes() : today.getMinutes();
+    var format = today.getHours()>= 12 ? "PM" : "AM";
+    if(today.getHours()>= 12)
+        hours = today.getHours() - 12 ;
+
+    document.getElementById("seconds").innerHTML = seconds;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("format").innerHTML = format;
+
+    setTimeout(() => {
+        updateClock();
+    }, 1000);
+}
+updateClock();
